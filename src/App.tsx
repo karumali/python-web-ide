ab346555c0a5dcf4fca95c3df48501ab63b7de6a
karumali <knyl2013@hotmail.com>
2024-10-20 21:42:20 -0400

    Add user menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css';
 import CodeEditor from './components/CodeEditor';
 import { auth, db, provider } from './firebaseConfig';
 import { Pyodide } from './types/pyodide';
+import { Button, Menu, MenuItem } from '@mui/material';
 
 
 type File = {
@@ -32,6 +33,7 @@ const App: React.FC = () => {
   const [isOutputVisible, setIsOutputVisible] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>(null);
   const [isCopied, setIsCopied] = useState<boolean>(false);
+  const [userMenuAnchorEl, setUserMenuAnchorEl] = useState<null | HTMLElement>(null);
 
   const setFiles = (files: File[]) => {
     files = files.map(x => ({
@@ -284,10 +286,10 @@ const App: React.FC = () => {
     } catch (error) {
       console.error('Login error:', error);
     }
+    handleUserMenuClose();
   };
 
   const logout = async () => {
-    if (!window.confirm('Are you sure you want to logout?')) return;
     try {
       await signOut(auth);
       setUser(null);
@@ -298,6 +300,7 @@ const App: React.FC = () => {
     } catch (error) {
       console.error('Logout error:', error);
     }
+    handleUserMenuClose();
   };
 
   const saveUserData = useCallback(async (files: File[]) => {
@@ -357,6 +360,16 @@ const App: React.FC = () => {
       });
     }
   };
+
+  const handleUserMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setUserMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleUserMenuClose = () => {
+    setUserMenuAnchorEl(null);
+  };
+
+  const userMenuOpen = Boolean(userMenuAnchorEl);
 
   return (
     <div className="workspace">
@@ -423,23 +436,39 @@ const App: React.FC = () => {
         >
           <svg width="22px" height="22px" viewBox="0 0 24 24" fill={isCopied ? "none" : "currentColor"} xmlns="http://www.w3.org/2000/svg">
             <path d="M8 5.00005C7.01165 5.00082 6.49359 5.01338 6.09202 5.21799C5.71569 5.40973 5.40973 5.71569 5.21799 6.09202C5 6.51984 5 7.07989 5 8.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.07989 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V8.2C19 7.07989 19 6.51984 18.782 6.09202C18.5903 5.71569 18.2843 5.40973 17.908 5.21799C17.5064 5.01338 16.9884 5.00082 16 5.00005M8 5.00005V7H16V5.00005M8 5.00005V4.70711C8 4.25435 8.17986 3.82014 8.5 3.5C8.82014 3.17986 9.25435 3 9.70711 3H14.2929C14.7456 3 15.1799 3.17986 15.5 3.5C15.8201 3.82014 16 4.25435 16 4.70711V5.00005"
-            stroke="currentColor" stroke-width={isCopied ? "2" : "0"} stroke-linecap="round" stroke-linejoin="round"/>
+              stroke="currentColor" stroke-width={isCopied ? "2" : "0"} stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </button>
         {user ? (
-          <button className="logout-button" onClick={logout} title="Logout">
+          <Button className="logout-button" onClick={handleUserMenuOpen} title="User Menu">
             <div
               className="avatar"
               style={{ backgroundColor: getAvatarColor(user.displayName || user.email || 'User') }}
             >
               {getInitials(user.displayName || user.email || 'User')}
             </div>
-          </button>
+          </Button>
         ) : (
-          <button className="login-button" onClick={login} title="Login">
+          <Button className="login-button" onClick={handleUserMenuOpen} title="Login">
             {defaultUserIcon}
-          </button>
+          </Button>
         )}
+        <Menu
+          id="user-menu"
+          anchorEl={userMenuAnchorEl}
+          open={userMenuOpen}
+          onClose={handleUserMenuClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          {
+            user && <MenuItem onClick={logout}>Logout</MenuItem>
+          }
+          {
+            !user && <MenuItem onClick={login}>Login</MenuItem>
+          }
+        </Menu>
       </div>
       {pyodide &&
         <div className="code-container">
