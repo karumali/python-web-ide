94d5a9053dd1af8ae594c3ef3195805cf658e7f5
karumali <knyl2013@hotmail.com>
2024-10-21 23:02:37 -0400

    Update app.spec.ts for creating new file test case

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -12,5 +12,14 @@ test.describe('Python Web IDE', () => {
     test('should exist main.py at the start', async ({ page }) => {
         await expect(page.locator('.tab.active span')).toHaveText('main.py');
     });
+
+    test('should allow creating a new file', async ({ page }) => {
+        await page.click('button[title="New File (Ctrl+N)"]');
+        const newFileName = 'test.py';
+        page.on('dialog', async dialog => {
+            await dialog.accept(newFileName);
+            await expect(page.locator('.tab.active span')).toHaveText(newFileName);
+        });
+    });
 });
 
