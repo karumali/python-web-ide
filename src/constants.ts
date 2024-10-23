1d11e18c5b29e99a610be66510e08d8cef4c64c6
karumali <knyl2013@hotmail.com>
2024-10-23 00:25:16 -0400

    Update completion endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const COMPLETION_ENDPOINT = 'https://aichathub.adaptable.app/api/complete';
-export const COMPLETION_ENDPOINT = 'http://localhost:3001/api/complete';
+export const COMPLETION_ENDPOINT = 'https://aichathub.adaptable.app/api/complete';
+// export const COMPLETION_ENDPOINT = 'http://localhost:3001/api/complete';
