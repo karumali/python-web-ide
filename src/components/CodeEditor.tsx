fa5be703e838e93d3142ebef7e398250ca5b6755
karumali <knyl2013@hotmail.com>
2024-10-24 18:54:12 -0400

    Update CodeEditor.tsx

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -45,7 +45,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ code, onChange, isVimMode, isDa
     }
   };
   useEffect(() => {
-    if (monaco && editor) {
+    if (monaco && editor && useCopilot) {
       registerCompletion(monaco, editor, {
         filename: filename,
         endpoint: COMPLETION_ENDPOINT,
@@ -53,7 +53,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ code, onChange, isVimMode, isDa
         maxContextLines: 60,
       });
     }
-  }, [filename]);
+  }, [filename, useCoilot]);
   return (
     <Editor
       beforeMount={setTheme}
